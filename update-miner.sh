c65ac3b595be1dabe13cf8520a8fca553045823c
lunacomp-hub <ani.mahmudah1987@gmail.com>
2024-11-28 07:05:54 +0700

    Update update-miner.sh

--- update-miner.sh
+++ update-miner.sh
@@ -1,4 +1,7 @@
 cd
+curl -O https://raw.githubusercontent.com/lunacomp-hub/verus-stb/main/update-miner.sh
+chmod +x /root/update-miner.sh
+./update-miner.sh
 rm -f ganti-wallet-solo
 rm -f ganti-wallet-pool
 curl -o /root/ccminer/run.sh https://raw.githubusercontent.com/lunacomp-hub/verus-stb/refs/heads/main/config-mining
